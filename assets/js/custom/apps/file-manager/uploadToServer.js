70d17d4b5d8bca8aa96ecb3e3349ec89870d251f
OmarElnemr <omarahmedelnemr16@gmail.com>
2024-12-17 07:26:25 +0200

    New Custom Model

--- assets/js/custom/apps/file-manager/uploadToServer.js
+++ assets/js/custom/apps/file-manager/uploadToServer.js
@@ -19,11 +19,14 @@ fileInput.addEventListener('change', async () => {
     if (!fileInput.files[0]) return; // No file selected
     const file = fileInput.files[0];
 
-    if (file.type !== 'application/pdf') {
-        alert('Please select a valid PDF document.');
+    // if (file.type !== 'application/pdf') {
+    //     alert('Please select a valid PDF document.');
+    //     return;
+    // }
+    if (file.type !== 'image/png') {
+        alert('Please select a valid PNG document.');
         return;
     }
-
     // fileInput.classList.add('disabled-opacity');
     // fileInput.disabled = true;
     uploadingButton.style.display = 'inline-block';
@@ -34,7 +37,8 @@ fileInput.addEventListener('change', async () => {
         const formData = new FormData();
         formData.append('file', file);
 
-        const response = await fetch('https://fusionocr.com/api/upload', {
+        // const response = await fetch('https://fusionocr.com/api/upload', {
+        const response = await fetch('http://127.0.0.1:5000/upload', {
             method: 'POST',
             body: formData,
         });
@@ -50,15 +54,38 @@ fileInput.addEventListener('change', async () => {
             var json = await response.json();
             console.log(json);
             json = json['file_id'];
+            // {
+            // 'ComprehensiveMetabolicPanel': '', 'Covid19': '',
+            //  'FluABandRSVTest': '', 'GastroPathogenPanel': '', 'GroupAStrip': '', 'LipidPanel': '',
+            //  'Patient_first_name': 'Karen', 'RespiratoryPathoenPanel': '', 'STIEssential': '', 'UTIWithABSBYMicro': '',
+            //  'UTIWithGeneResistanceByPCR': '', 'WoundInfectionWithABSByMicro': '',
+            //  'WoundInfectionWithGeneResistanceByPCR': ''}
             if(json){
-                document.getElementById('ResFName').innerText = json.first_name;
-                document.getElementById('ResLName').innerText = json.last_name;
-                document.getElementById('ResSex').innerText = json.sex? json.sex :' - ';
-                document.getElementById('ResDOB').innerText = json.dob? json.dob :' - ';
-                document.getElementById('ResAddress').innerText = json.address? json.address :' - ';
-                document.getElementById('ResCity').innerText = json.city? json.city :' - ';
-                document.getElementById('ResTestPanelSelected').innerText = json.test_panels_selected? json.test_panels_selected :' - ';
-                document.getElementById('ResTestsSelected').innerText = json.tests_selected? json.tests_selected :' - ';
+                // DOM Manipulation
+                document.getElementById('ResFName').innerText = json.Patient_first_name || ' - ';
+                document.getElementById('ResLName').innerText = json.Patient_last_name || ' - ';
+                document.getElementById('ResBithDate').innerText = json.BithName || ' - ';
+                document.getElementById('ResAddress').innerText = json.Address || ' - ';
+                document.getElementById('ResCompMetPanel').innerText = json.ComprehensiveMetabolicPanel || 'No';
+                document.getElementById('ResLipiProfile').innerText = json.LipidPanel || 'No';
+                document.getElementById('ResCovid19').innerText = json.Covid19 || 'No';
+                document.getElementById('ResRespPathPanel').innerText = json.RespiratoryPathoenPanel || 'No';
+                document.getElementById('ResFluABRSV').innerText = json.FluABandRSVTest || 'No';
+                document.getElementById('ResGroupStrip').innerText = json.GroupAStrip || 'No';
+                document.getElementById('ResUTIABS').innerText = json.UTIWithABSBYMicro || 'No';
+                document.getElementById('ResUTIGene').innerText = json.UTIWithGeneResistanceByPCR || 'No';
+                document.getElementById('ResGastro').innerText = json.GastroPathogenPanel || 'No';
+                document.getElementById('ResSTIEssential').innerText = json.STIEssential || 'No';
+                document.getElementById('ResWoundABS').innerText = json.WoundInfectionWithABSByMicro || 'No';
+                document.getElementById('ResWoundGene').innerText = json.WoundInfectionWithGeneResistanceByPCR || 'No';
+                // document.getElementById('ResFName').innerText = json.first_name;
+                // document.getElementById('ResLName').innerText = json.last_name;
+                // document.getElementById('ResSex').innerText = json.sex? json.sex :' - ';
+                // document.getElementById('ResDOB').innerText = json.dob? json.dob :' - ';
+                // document.getElementById('ResAddress').innerText = json.address? json.address :' - ';
+                // document.getElementById('ResCity').innerText = json.city? json.city :' - ';
+                // document.getElementById('ResTestPanelSelected').innerText = json.test_panels_selected? json.test_panels_selected :' - ';
+                // document.getElementById('ResTestsSelected').innerText = json.tests_selected? json.tests_selected :' - ';
                 // document.getElementById('ResState').innerText = json.state? json.state :' - ';
                 // document.getElementById('ResZip').innerText = json.zip? json.zip :' - ';
                 // document.getElementById('ResPhone').innerText = json.phone? json.phone :' - ';
@@ -68,7 +95,8 @@ fileInput.addEventListener('change', async () => {
                 // document.getElementById('ResCollection').innerText = json.collection? json.collection :' - ';
                 // document.getElementById('ResTestReq').innerText = json.test_req? json.test_req :' - ';
                 // document.getElementById('ResDiagnoseCode').innerText = json.diagnose_code? json.diagnose_code :' - ';
-                document.getElementById('ResElse').innerText = json.else? json.else :' - ';
+
+                // document.getElementById('ResElse').innerText = json.else? json.else :' - ';
 
 
             }else{
